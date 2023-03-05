75e8c97d717c99645c135e251c482cc625a67c7c
Brandon Taft <btaftcan@gmail.com>
2023-03-04 22:41:41 -0500

    fixed highliting

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import AutoComplete from './lib/AutoComplete';
 
 function App() {
   //let items = [{ a: 'we' }, { a: 'wer' }, { a: 'wert' }]
-  let items = ['hey', 'hell', 'hello', 'help', 'helio']
+  let items = ['hey', 'hell', 'hello', 'help', 'helio', 'her', 'herby']
   return (
     <div className="App">
       <AutoComplete
         //propValue={(item1) => item1.a}
         list={items}
+        highlightFirstItem={true}
       />
     </div>
   );
--- src/lib/AutoComplete.jsx
+++ src/lib/AutoComplete.jsx
@@ -2,73 +2,102 @@ import { useState, useEffect, useRef } from "react";
 import Trie from "./trie";
 
 
-export default function AutoComplete({ list, propValue }) {
+export default function AutoComplete({ list, propValue, highlightFirstItem }) {
   const [trie, setTrie] = useState();
-  const [cached, setCached] = useState(false)
+  const [isCached, setIsCached] = useState(false);
+  const [isHighlighted, setIsHighlighted] = useState(Number(highlightFirstItem - 1))
   const [suggestedWords, setSuggestedWords] = useState([]);
-  const cursor = useRef(0);
+  const cursor = useRef();
+  const listRef = useRef();
+  const highlitedRef = useRef(null);
 
   useEffect(() => {
     setTrie(new Trie())
-    setCached(false)
+    setIsCached(false)
   }, [])
 
+  // Getting and Setting list items
   let listItems;
   if (!propValue) {
     listItems = list
   } else {
     listItems = list.map(propValue)
   };
-  //If the Node isn't already initialized and once the list prop is loaded
-  if (listItems && !cached && trie) {
+
+  //If the Node isn't already initialized and the list prop is loaded
+  if (listItems && !isCached && trie) {
     for (let i = 0; i < listItems.length; i++) {
       const item = listItems[i]
       trie.insert(item)
     }
-    setCached(true)
+    setIsCached(true)
   }
 
   const handlePrefix = (e) => {
-    if(e.target.value === null) {
-       cursor.current = 0
-    }
-    console.log(cursor.current)
+    // cursor.current = refValue
     const prefix = e.target.value
     if (prefix.length > 0) {
       setSuggestedWords(trie.find(e.target.value))
     } else {
       setSuggestedWords([])
     }
-  }
-  
-  const handleKeyDown = (e) => {
-    if (e.keyCode === 40 && cursor.current < suggestedWords.length - 1) {
-      e.preventDefault()
-      cursor.current++
-      document.getElementById(`suggested-word-${cursor.current}`).style.backgroundColor = "blue"
-      document.getElementById(`suggested-word-${cursor.current - 1}`).style.backgroundColor = "white"
-    }
-    if (e.keyCode === 38 && cursor.current > 0) {
-      e.preventDefault()
-      cursor.current--
-      document.getElementById(`suggested-word-${cursor.current}`).style.backgroundColor = "blue"
-      document.getElementById(`suggested-word-${cursor.current + 1}`).style.backgroundColor = "white"
-    }
-    if (e.keyCode === 13 && cursor.current >= 0) {
-      e.preventDefault()
-      const selectedItem = document.getElementById(`suggested-word-${cursor.current}`)
-      setSuggestedWords([])
-      if (selectedItem) {
-        e.target.value = selectedItem.innerHTML.valueOf()
-      }
-    }
+    
   }
 
+  const handleKeyDown = (e) => { 
+    if (e.keyCode === 40 ) {
+      if(isHighlighted < suggestedWords.length - 1) {
+     setIsHighlighted( isHighlighted + 1 )
+      }
+    };
+    if (e.keyCode === 38 ) {
+      if(isHighlighted > 0) {
+     setIsHighlighted( isHighlighted - 1 )
+      }
+    };
+
+  }
+
+  // const handleKeyDown = (e) => { 
+  //   if (e.target.value === "") {
+  //     cursor.current = refValue
+  //   }
+  //   if (e.keyCode === 40 && cursor.current < suggestedWords.length - 1) {
+  //     e.preventDefault()
+  //     cursor.current++
+  //     document.getElementById(`suggested-word-${cursor.current}`).style.backgroundColor = "blue"
+  //     if(cursor.current > 0){
+  //     document.getElementById(`suggested-word-${cursor.current - 1}`).style.backgroundColor = "inherit"
+  //     }
+  //   }
+  //   if (e.keyCode === 38 && cursor.current > 0) {
+  //     e.preventDefault()
+  //     cursor.current--
+  //     document.getElementById(`suggested-word-${cursor.current}`).style.backgroundColor = "blue"
+  //     document.getElementById(`suggested-word-${cursor.current + 1}`).style.backgroundColor = "inherit"
+  //   }
+  //   if (e.keyCode === 13 && cursor.current >= 0) {
+  //     e.preventDefault()
+  //     const selectedItem = document.getElementById(`suggested-word-${cursor.current}`)
+  //     setSuggestedWords([])
+  //     if (selectedItem) {
+  //       e.target.value = selectedItem.innerHTML.valueOf()
+  //     }
+  //   }
+  // }
+
   const suggestedWordList = suggestedWords.map((suggestedWord, index) => {
+    if(isHighlighted > suggestedWords.length) {
+      setIsHighlighted(0)
+    }
     return (
-      <li key={index} id={`suggested-word-${index}`} style={cursor.current === index ? { backgroundColor: 'blue' } : {backgroundColor: 'none'}}>
+      <div key={index} id={`suggested-word-${index}`} style={{ background: isHighlighted === index ? 'lightgray' : 'white' }}>
         {suggestedWord}
-      </li>
+      </div>
+
+      // <div key={index} id={`suggested-word-${index}`} style={cursor.current === index ? { backgroundColor: 'blue' } : { backgroundColor: 'none' }}>
+      //   {suggestedWord}
+      // </div>
     )
   })
 
@@ -83,7 +112,7 @@ export default function AutoComplete({ list, propValue }) {
         onKeyDown={handleKeyDown}
         autoComplete='off'
       />
-      <div className="search-list">
+      <div className="search-list" >
         {suggestedWordList}
       </div>
     </>
